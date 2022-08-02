f25c1e9026835f30d1d6bf72a4dad9a289a14b5e
ecowan <ethan24@gmail.com>
2022-08-02 00:30:26 -0400

    Using find-packages

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
+from setuptools import find_packages
+
 
 setup(name='pjg_sound',
       version='0.1',
@@ -8,5 +10,5 @@ setup(name='pjg_sound',
       author='Ethan Cowan',
       author_email='ethanc@poeticjustice.group',
       url='',
-      packages=['pjg_sound'],
+      packages=find_packages(),
 )
