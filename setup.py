9275f758fd66872a1734e0bde79cf3126a8ac96a
ecowan <ethan24@gmail.com>
2022-08-02 00:15:55 -0400

    Adding setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='pjg-sound-processing',
+      version='0.1',
+      description='Sound processing utilities',
+      author='Ethan Cowan',
+      author_email='ethanc@poeticjustice.group',
+      url='',
+      packages=['pjg-sound'],
+)
